0d176727d00ec0cc9846bf35a03100a43ecf60e6
lauracali1 <lauracali541@gmail.com>
2024-01-04 15:52:53 +0100

    Iliria Palace

---
+++ js/jswebsite.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+	$(window).on('scroll',function(){
+		if($(window).scrollTop()){
+		   $("header").addClass('bgc');
+		}else{
+		   $("header").removeClass('bgc');
+		}
+	 });
+});
+
+function openPage(evt,pageName){
+var i,tabcontent,tablinks;
+tabcontent=document.getElementsByClassName("tabcontent");
+for(i=0; i<tabcontent.length;i++){
+tabcontent[i].style.display="none";
+}
+tablinks=document.getElementsByClassName("tablinks");
+for(i=0; i<tablinks.length;i++){
+  tablinks[i].className=tablinks[i].className.replace("active","");
+}
+document.getElementById(pageName).style.display="block";
+evt.currentTarget.className +="active";
+}
+document.getElementById("defaultOpen").click();
